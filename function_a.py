38086290d3c361816214909cd7e50b18ff9d83b4
Amanda <jonesamanda996@gmail.com>
2023-03-24 13:55:58 -0400

    friyay

--- function_a.py
+++ function_a.py
@@ -2,7 +2,7 @@ def most_common_value(number_list):
     """ returns the most common element of the list
     """
     frequency_index = {}
-    max_frequency = -1
+    max_frequency = 2
     most_common_value = None
     for num in number_list:
         if frequency_index.get(num):
